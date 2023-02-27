8e8cf1e4228a21120dbf52223dda20e50a291635
yjyjy15 <122818606+yjyjy15@users.noreply.github.com>
2023-02-28 04:37:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -114,7 +114,7 @@ esac
 # 是否自己编译
 if [[ -z $not_rebuild ]]; then
     echo
-    echo -e "${yellow}本系统架构是${magenta}${SYSTEM_ARCH}${none}${yellow}, 你同意直接下载NaïveProxy作者编译好的Caddy吗?${none}"
+    echo -e "${yellow}本系统架构是${magenta}${SYSTEM_ARCH}${none}${yellow}, 你同意直接下载lxhao61编译好的Caddy吗?${none}"
     echo -e "${magenta}Y${none}, 使用编译好的Caddy; ${magenta}n${none}, 重新编译. (直接回车默认${magenta}Y${none})"
     while :; do 
         read -p "(Y/n): " not_rebuild
@@ -136,10 +136,9 @@ if [[ "$not_rebuild" == [yY] ]]; then
     echo "----------------------------------------------------------------"
     cd /tmp
     rm caddy-linux-amd64.tar.gz
-    rm -r caddy-linux-amd64
+    rm caddy
     wget https://github.com/lxhao61/integrated-examples/releases/download/20230221/caddy-linux-amd64.tar.gz
     tar -xf caddy-linux-amd64.tar.gz
-    cd caddy-linux-amd64
     ./caddy version
 elif [[ "$not_rebuild" == [nN] ]]; then
     echo
@@ -162,195 +161,17 @@ cp caddy /usr/bin/
 mkdir -p /var/www/html
 echo "hello world" > /var/www/html/index.html
 
-# 域名
-if [[ -z $naive_domain ]]; then
-    while :; do
-        echo
-        echo -e "请输入一个 ${magenta}正确的域名${none} Input your domain"
-        read -p "(例如: mydomain.com): " naive_domain
-        [ -z "$naive_domain" ] && error && continue
-        echo
-        echo
-        echo -e "$yellow 你的域名Domain = $cyan$naive_domain$none"
-        echo "----------------------------------------------------------------"
-        break
-    done
-fi
 
-# 网络栈
-if [[ -z $netstack ]]; then
-    echo -e "如果你的小鸡是${magenta}双栈(同时有IPv4和IPv6的IP)${none}，请选择你把v2ray搭在哪个'网口'上"
-    echo "如果你不懂这段话是什么意思, 请直接回车"
-    read -p "$(echo -e "Input ${cyan}4${none} for IPv4, ${cyan}6${none} for IPv6:") " netstack
-    if [[ $netstack == "4" ]]; then
-        domain_resolve=$(curl -sH 'accept: application/dns-json' "https://cloudflare-dns.com/dns-query?name=$naive_domain&type=A" | jq -r '.Answer[0].data')
-    elif [[ $netstack == "6" ]]; then 
-        domain_resolve=$(curl -sH 'accept: application/dns-json' "https://cloudflare-dns.com/dns-query?name=$naive_domain&type=AAAA" | jq -r '.Answer[0].data')
-    else
-        domain_resolve=$(curl -sH 'accept: application/dns-json' "https://cloudflare-dns.com/dns-query?name=$naive_domain&type=A" | jq -r '.Answer[0].data')
-        if [[ "$domain_resolve" != "null" ]]; then
-            netstack="4"
-        else
-            domain_resolve=$(curl -sH 'accept: application/dns-json' "https://cloudflare-dns.com/dns-query?name=$naive_domain&type=AAAA" | jq -r '.Answer[0].data')            
-            if [[ "$domain_resolve" != "null" ]]; then
-                netstack="6"
-            fi
-        fi
-    fi
-
-    # 本机 IP
-    if [[ $netstack == "4" ]]; then
-        ip=$(curl -4s https://api.myip.la)
-    elif [[ $netstack == "6" ]]; then 
-        ip=$(curl -6s https://api.myip.la)
-    else
-        ip=$(curl -s https://api.myip.la)
-    fi
-
-    if [[ $domain_resolve != $ip ]]; then
-        echo
-        echo -e "$red 域名解析错误Domain resolution error....$none"
-        echo
-        echo -e " 你的域名: $yellow$domain$none 未解析到: $cyan$ip$none"
-        echo
-        if [[ $domain_resolve != "null" ]]; then
-            echo -e " 你的域名当前解析到: $cyan$domain_resolve$none"
-        else
-            echo -e " $red检测不到域名解析Domain not resolved $none "
-        fi
-        echo
-        echo -e "备注...如果你的域名是使用$yellow Cloudflare $none解析的话... 在 DNS 设置页面, 请将$yellow代理状态$none设置为$yellow仅限DNS$none, 小云朵变灰."
-        echo "Notice...If you use Cloudflare to resolve your domain, on 'DNS' setting page, 'Proxy status' should be 'DNS only' but not 'Proxied'."
-        echo
-        exit 1
-    else
-        echo
-        echo
-        echo -e "$yellow 域名解析 = ${cyan}我确定已经有解析了$none"
-        echo "----------------------------------------------------------------"
-        echo
-    fi
-fi
-
-# 端口
-if [[ -z $naive_port ]]; then
-    default=443
-    while :; do
-        echo -e "请输入 ${yellow}端口${none} [${magenta}1-65535${none}], 不能选择 ${magenta}80${none}端口"
-        read -p "$(echo -e "(默认端口port: ${cyan}${default}$none):")" naive_port
-        [ -z "$naive_port" ] && naive_port=$default
-        case $naive_port in
-        80)
-            echo
-            echo " ...都说了不能选择 80 端口了咯....."
-            error
-            ;;
-        [1-9] | [1-9][0-9] | [1-9][0-9][0-9] | [1-9][0-9][0-9][0-9] | [1-5][0-9][0-9][0-9][0-9] | 6[0-4][0-9][0-9][0-9] | 65[0-4][0-9][0-9] | 655[0-3][0-5])
-            echo
-            echo
-            echo -e "$yellow 端口Port = $cyan$naive_port$none"
-            echo "----------------------------------------------------------------"
-            echo
-            break
-            ;;
-        *)
-            error
-            ;;
-        esac
-    done
-fi
-
-# 用户名
-if [[ -z $naive_user ]]; then
-    random=$(openssl rand -hex 8)
-    while :; do
-        echo
-        echo -e "请输入 ${magenta}用户名${none} Input your username"
-        read -p "$(echo -e "(默认: ${cyan}${random}$none):") " naive_user
-        [ -z "$naive_user" ] && naive_user=$random
-        echo
-        echo
-        echo -e "$yellow 你的用户名Username = $cyan$naive_user$none"
-        echo "----------------------------------------------------------------"
-        break
-    done
-fi
-
-# 密码
-if [[ -z $naive_pass ]]; then
-    random=$(openssl rand -hex 8)
-    while :; do
-        echo
-        echo -e "请输入 ${magenta}密码${none} Input your password"
-        read -p "$(echo -e "(默认: ${cyan}${random}$none):") " naive_pass
-        [ -z "$naive_pass" ] && naive_pass=$random
-        echo
-        echo
-        echo -e "$yellow 你的密码Password = $cyan$naive_pass$none"
-        echo "----------------------------------------------------------------"
-        break
-    done
-fi
-
-# # 修改Caddyfile
-# echo
-# echo -e "$yellow修改Caddyfile$none"
-# echo "----------------------------------------------------------------"
-# begin_line=$(awk "/_naive_config_begin_/{print NR}" /etc/caddy/Caddyfile)
-# end_line=$(awk "/_naive_config_end_/{print NR}" /etc/caddy/Caddyfile)
-# if [[ -n $begin_line && -n $end_line ]]; then
-#   sed -i "${begin_line},${end_line}d" /etc/caddy/Caddyfile
-# fi
-# 
-# sed -i "1i # _naive_config_begin_\n\
-# {\n\
-#   order forward_proxy before file_server\n\
-# }\n\
-# :${naive_port}, ${naive_domain}:${naive_port} {\n\
-#   tls sample@email.com\n\
-#   forward_proxy {\n\
-#     basic_auth ${naive_user} ${naive_pass}\n\
-#     hide_ip\n\
-#     hide_via\n\
-#     probe_resistance\n\
-#   }\n\
-#   file_server {\n\
-#     root /var/www/html\n\
-#   }\n\
-# }\n\
-# # _naive_config_end_" /etc/caddy/Caddyfile
 
 # 启动NaïveProxy服务端(Caddy)
 echo
-echo -e "$yellow启动NaïveProxy服务端(Caddy)$none"
+echo -e "$yellow请在配置caddyfile或caddy.json后启动NaïveProxy服务端(service caddy start)$none"
 echo "----------------------------------------------------------------"
-service caddy start
+# service caddy start
 
-# # 输出参数
-# echo
-# echo -e "${yellow}NaïveProxy配置参数${none}"
-# echo "----------------------------------------------------------------"
-# echo -e "域名Domain: ${naive_domain}"
-# echo -e "端口Port: ${naive_port}"
-# echo -e "用户名Username: ${naive_user}"
-# echo -e "密码Password: ${naive_pass}"
 
-# naive_url="https://$(echo -n \
-# "${naive_user}:${naive_pass}@${naive_domain}:${naive_port}" \
-# | base64 -w 0)"
-# echo -e "${cyan}${naive_url}${none}"
-# echo "以下两个二维码完全一样的内容"
-# qrencode -t UTF8 $naive_url
-# qrencode -t ANSI $naive_url
 
 echo "---------- END -------------"
-# echo "以上节点信息保存在 ~/_naive_url_ 中"
-
-# echo $naive_url > ~/_naive_url_
-# echo "以下两个二维码完全一样的内容" >> ~/_naive_url_
-# qrencode -t UTF8 $naive_url >> ~/_naive_url_
-# qrencode -t ANSI $naive_url >> ~/_naive_url_
-
 echo
 echo "----------------------------------------------------------------"
 echo "END"
